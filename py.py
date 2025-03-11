c3f56c11a55794af2a834426922cab8b5569de22
unknown <makar.simonov@infis.cz>
2025-03-11 09:06:34 +0100

    commit

---
+++ py.py
@@ -0,0 +1,10 @@
+# a = input()                načteni
+# a = int(a)                 přetypovani
+# if a  == 2 and a == 4:     podminky
+#     pass
+# elif a == 3:               podminky
+#     pass
+#     print('gg')            vypis
+# else:                      podminky
+#     pass
+
